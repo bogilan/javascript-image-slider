fd45a3f560fcac92e49ea299eafb19c4a38e46e8
bogilan <bogdan.lakicevic@gmail.com>
2023-03-09 22:25:41 +0100

    Simplified version of JavaScript code

--- script.js
+++ script.js
@@ -10,72 +10,47 @@ radioBtns[0].checked = true;
 prevBtn.addEventListener('click', () => {
   // Get index of image with class 'active'
   let activeIndex = Array.from(images).findIndex((img) => img.classList.contains('active'));
-  if(activeIndex === 0) { // if index is 0, which is first img
-    activeIndex = images.length - 1; // set active index to last img element
-  }
-  else {
-    activeIndex--; // else decrease index by 1
-  }
+  // if index is 0 (which is first img) set active index to last img element, else decrease by 1
+  activeIndex = activeIndex === 0 ? images.length - 1 : activeIndex - 1;
   // call setImage and checkRadioBtns functions on Active Index
   setImage(activeIndex);
   checkRadioBtns(radioBtns, activeIndex);
 });
 
-
 nextBtn.addEventListener('click', () => {
   // get index of 'active'
   let activeIndex = Array.from(images).findIndex((img) => img.classList.contains('active'));
-  if(activeIndex === images.length - 1) { // if last image
-    activeIndex = 0; // return index to 0 which is first img
-  }
-  else {
-    activeIndex++; // else increase index by 1
-  }
+  // if last image, return index to 0 (first img), else increase index by 1
+  activeIndex = activeIndex === images.length - 1 ? 0 : activeIndex + 1;
   // call functions
   setImage(activeIndex);
   checkRadioBtns(radioBtns, activeIndex);
 });
 
-// setImage function takes index as input
+// setImage function takes activeIndex as input
 function setImage(activeIndex) {
-  images.forEach((img, i) => { // loops through all images
-    if(i === activeIndex) { // where img index is equal to activeIndex
-      img.classList.add('active'); // set class to 'active'
-    }
-    else {
-      img.classList.remove('active'); // else remove 'active' class
-    }
-  })
+  // loops through all images and remove active class
+  images.forEach(img => { img.classList.remove('active')});
+  // add active class to image with activeIndex number
+  images[activeIndex].classList.add('active');
 }
 
-// checkRadioBtns function takes all radio btns and index as input
+// checkRadioBtns function takes activeIndex as input
 function checkRadioBtns(radioBtns, activeIndex) {
-  radioBtns.forEach((radioBtn, i) => { // loop through radio buttons
-    if (i === activeIndex) { // where index is equal to activeIndex
-      radioBtn.checked = true; // check radio btn
-    } else {
-      radioBtn.checked = false; // else deselect btn
-    }
-  });
+  // loops through all radio buttons and their indexes
+  radioBtns.forEach((radioBtn, index) => {
+    // if index is equal to activeIndex check button else uncheck
+    radioBtn.checked = index === activeIndex ? true : false; 
+  });  
 }
 
-// change image on selecting radio btn
-for (let i = 0; i < radioBtns.length; i++) { // loop through all radio btns
-  const radioBtn = radioBtns[i]; // store radio btn
-  const image = images[i]; // and following img
-  radioBtn.addEventListener('change', ()=> { // on change btn event
-    images.forEach(img => { // remove class 'active' from all images
-      img.classList.remove('active');
-    })
-    if (radioBtn.checked) { // if radio btn is checked
-      image.classList.add('active'); // add 'active' class to img
-      radioBtns.forEach((btn, index) => { // loop through radio btns
-        if(index != i) { // and uncheck all btns whose index is different from active index
-          btn.checked = false;
-        }
-      })
-    }
+// for each radio button and following index
+radioBtns.forEach((radioBtn, index) => {
+  radioBtn.addEventListener('change', ()=> { // add 'change' event listener
+    images.forEach(img => img.classList.remove('active')); // remove active class from all images
+    images[index].classList.add('active'); // add active class to image with index of clicked button
+    checkRadioBtns(radioBtns, index); // call checkRadioBtns function in order to check only radio button with active index
   })
-}
+})
 
 }
